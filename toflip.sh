c4e5c4314af90d0885b52b669bdd73f7227ea325
teemal <taymal1987@gmail.com>
2019-02-21 12:23:13 -0800

    Init commit

---
+++ toflip.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+FILE_NUM=""
+OPTIND=1
+FROM=$(PWD)
+TO='mallorta@flip.engr.oregonstate.edu:~/cs344'
+while getopts "hn:" opt; do
+    case "$opt" in
+    h)  echo "run script to tranfer files to flip server with -n N to denote lab number"
+		exit 0
+		;;
+    n)  FILE_NUM=$OPTARG
+        ;;
+    esac
+done
+
+echo "from: ${FROM}/lab${FILE_NUM}"
+echo "to: ${TO}"
+
+scp -r ${FROM}/lab${FILE_NUM} ${TO}
